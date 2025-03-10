1affc9525e84c08d8a3c7d2d848fd433fb94d8ab
DominikKoperkiewicz <62205572+DominikKoperkiewicz@users.noreply.github.com>
2025-03-10 23:35:40 +0100

    Walk animation added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,22 @@
+using Spine.Unity;
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public Animator animator;
+    public Rigidbody2D entityRigidbody;
+
     [SerializeField] private PlayerInputActions playerInputActions;
-    [SerializeField] private Rigidbody2D rb;
+    //[SerializeField] private SkeletonAnimation skeletonAnimation;
     [SerializeField] private float movementSpeed = 5.0f;
+
+    [Header("Animations")]
+    private bool isFliped = false;
+    [SerializeField] private AnimationReferenceAsset idleAnimation;
+    [SerializeField] private AnimationReferenceAsset walkAnimation;
+
 
     private Vector2 moveDirection;
 
@@ -30,6 +40,41 @@ public class Player : MonoBehaviour
 
     private void Move(InputAction.CallbackContext context)
     {
-        rb.linearVelocityX = context.ReadValue<float>() * movementSpeed;
+        float horizontalVelocity = context.ReadValue<float>() * movementSpeed;
+
+        entityRigidbody.linearVelocityX = horizontalVelocity;
+        //skeletonAnimation.AnimationState.SetAnimation(0, walkAnimation, true);
+        //skeletonAnimation.AnimationState.AddAnimation(0, idleAnimation, true, 0f);
+
+        if (horizontalVelocity < 0)
+        {
+            animator.SetBool("isWalking", true);
+            SetFlip(true);
+        }
+        else if (horizontalVelocity > 0) 
+        {
+            animator.SetBool("isWalking", true);
+            SetFlip(false);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    public void SetFlip(bool FlipValue)
+    {
+        if (isFliped != FlipValue) 
+        {
+            isFliped = !isFliped;
+            Vector3 animatorCurrentScale = animator.transform.localScale;
+            animatorCurrentScale.x *= -1;
+            animator.transform.localScale = animatorCurrentScale;
+        }
+    }
+
+    public void Cough() 
+    {
+        animator.SetTrigger("Cough");
     }
 }
